6c54d5aeef1bd17305b9f4f0d520f824c73a7779
k-washi <bandad.kw@gmail.com>
2024-07-13 12:26:11 +0900

    add model

--- hubconf.py
+++ hubconf.py
@@ -26,24 +26,6 @@ def ecapatdnn_ja_l512(progress: bool = True, pretrained: bool = True) -> _Speake
         param.requires_grad = False
     return model
 
-
-def ecapatdnn_ja_l128_clean(progress: bool = True, pretrained: bool = True) -> _SpeakerEmbeddingJa:
-    model = _SpeakerEmbeddingJa(hidden_size=128)
-    if pretrained:
-        output_fp = Path("/tmp/speaker-emb-ja-ecapa-tdnn-l512.pth")
-        gdown.download(
-            "https://drive.google.com/u/1/uc?id=1Qa0lqrKduUCJzagqe59fQ5R8-xQmeIVG",
-            str(output_fp),
-            quiet=False
-        )
-        model.model.load_state_dict(torch.load(output_fp))
-    
-    model.model.eval()
-    model.preprocess.eval()
-    for param in model.model.parameters():
-        param.requires_grad = False
-    return model
-
 def ecapatdnn_ja_l128_clean(progress: bool = True, pretrained: bool = True) -> _SpeakerEmbeddingJa:
     model = _SpeakerEmbeddingJa(hidden_size=128)
     if pretrained:
